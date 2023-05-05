be635cf40e0f85b3d68b3e77448ef9a46404ca98
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-05-05 18:04:35 +0300

    Interrupt function added in M.2 Key-B test

--- test_key_b_agx_orin.sh
+++ test_key_b_agx_orin.sh
@@ -12,12 +12,16 @@ sudo echo high > /sys/class/gpio/PA.01/direction
 #sudo echo high > /sys/class/gpio/PQ.04/direction
 #sudo echo high > /sys/class/gpio/PP.04/direction
 
+trap interrupt_func INT
+interrupt_func() {
+	sudo echo high > /sys/class/gpio/PX.06/direction
+
+	#sudo echo 446 > /sys/class/gpio/unexport
+	sudo echo 349 > /sys/class/gpio/unexport
+	#sudo echo 350 > /sys/class/gpio/unexport
+	#sudo echo 452 > /sys/class/gpio/unexport
+	#sudo echo 444 > /sys/class/gpio/unexport 
+}
+
 watch -n 0.1 lsusb
 
-sudo echo high > /sys/class/gpio/PX.06/direction
-
-#sudo echo 446 > /sys/class/gpio/unexport
-sudo echo 349 > /sys/class/gpio/unexport
-#sudo echo 350 > /sys/class/gpio/unexport
-#sudo echo 452 > /sys/class/gpio/unexport
-#sudo echo 444 > /sys/class/gpio/unexport 
