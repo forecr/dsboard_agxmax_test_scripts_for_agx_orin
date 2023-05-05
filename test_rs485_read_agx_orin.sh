d5fffceffe5554dd1a60288bef9e5dc8dc788e11
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-05-05 17:57:35 +0300

    RS485 Half/Full setting problem fixed

--- test_rs485_read_agx_orin.sh
+++ test_rs485_read_agx_orin.sh
@@ -9,10 +9,10 @@ sudo echo 485 > /sys/class/gpio/export
 sudo echo 344 > /sys/class/gpio/export 
 sudo echo 428 > /sys/class/gpio/export 
 sudo echo 433 > /sys/class/gpio/export 
-sudo echo high > /sys/class/gpio/PZ.07/direction 
-sudo echo low > /sys/class/gpio/PEE.05/direction 
-sudo echo low > /sys/class/gpio/PM.04/direction 
-sudo echo low > /sys/class/gpio/PN.01/direction 
+sudo echo high > /sys/class/gpio/PZ.07/direction
+sudo echo high > /sys/class/gpio/PEE.05/direction
+sudo echo low > /sys/class/gpio/PM.04/direction
+sudo echo low > /sys/class/gpio/PN.01/direction
 
 sudo gtkterm -p /dev/ttyTHS4 -s 115200 -w RS485
 
