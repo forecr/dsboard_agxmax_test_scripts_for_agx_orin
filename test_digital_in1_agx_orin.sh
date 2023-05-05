c8945bb1d63cffe445f2643938ed45e912db4478
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-05-05 17:49:54 +0300

    Interrupt functions added in digital-in scripts

--- test_digital_in1_agx_orin.sh
+++ test_digital_in1_agx_orin.sh
@@ -7,6 +7,10 @@ fi
 sudo echo 324 > /sys/class/gpio/export
 sudo echo in > /sys/class/gpio/PBB.00/direction
 
+trap interrupt_func INT
+interrupt_func() {
+	sudo echo 324 > /sys/class/gpio/unexport
+}
+
 watch -n 0.1 sudo cat /sys/class/gpio/PBB.00/value
 
-sudo echo 324 > /sys/class/gpio/unexport
