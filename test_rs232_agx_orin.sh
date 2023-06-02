ea4b77148db8cd58491b0a3340c3585be88e6c90
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-06-02 12:29:20 +0300

    RS232 TX problem solved

--- test_rs232_agx_orin.sh
+++ test_rs232_agx_orin.sh
@@ -7,8 +7,12 @@ fi
 
 sudo echo 485 > /sys/class/gpio/export 
 sudo echo 344 > /sys/class/gpio/export 
+sudo echo 428 > /sys/class/gpio/export
+sudo echo 433 > /sys/class/gpio/export
 sudo echo low > /sys/class/gpio/PZ.07/direction 
 sudo echo low > /sys/class/gpio/PEE.05/direction 
+sudo echo high > /sys/class/gpio/PM.04/direction
+sudo echo low > /sys/class/gpio/PN.01/direction
 
 sudo gtkterm -p /dev/ttyTHS4 -s 115200
 
