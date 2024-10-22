7f902c812a20341aad88794f1cbe063ffca7445d
mdeniz0 <mehmet@forecr.io>
2024-10-22 16:16:15 +0300

    M.2 key-B script files updated. USB switch GPIO changed for the newer ones (since rev-1.2)

--- JetPack-5/test_key_b_agx_orin.sh
+++ JetPack-5/test_key_b_agx_orin.sh
@@ -1,11 +1,20 @@
-sudo echo 468 > /sys/class/gpio/export #Key-B select
+#Key-B select
+# rev-1.2 or newer
+sudo echo 474 > /sys/class/gpio/export
+# rev-1.1 or older
+#sudo echo 468 > /sys/class/gpio/export
+
 #sudo echo 446 > /sys/class/gpio/export
 sudo echo 349 > /sys/class/gpio/export
 #sudo echo 350 > /sys/class/gpio/export
 #sudo echo 452 > /sys/class/gpio/export
 #sudo echo 444 > /sys/class/gpio/export
 
-sudo echo low > /sys/class/gpio/PX.06/direction
+# rev-1.2 or newer
+sudo echo low > /sys/class/gpio/PY.04/direction
+# rev-1.1 or older
+#sudo echo low > /sys/class/gpio/PX.06/direction
+
 #sudo echo low > /sys/class/gpio/PP.06/direction
 sudo echo high > /sys/class/gpio/PA.01/direction
 #sudo echo high > /sys/class/gpio/PA.02/direction
@@ -14,9 +23,12 @@ sudo echo high > /sys/class/gpio/PA.01/direction
 
 trap interrupt_func INT
 interrupt_func() {
-	sudo echo high > /sys/class/gpio/PX.06/direction
+	sudo echo high > /sys/class/gpio/PY.04/direction
+	sudo echo 474 > /sys/class/gpio/unexport
+	# rev-1.1 or older
+	#sudo echo high > /sys/class/gpio/PX.06/direction
+	#sudo echo 468 > /sys/class/gpio/unexport
 
-	sudo echo 468 > /sys/class/gpio/unexport
 	#sudo echo 446 > /sys/class/gpio/unexport
 	sudo echo 349 > /sys/class/gpio/unexport
 	#sudo echo 350 > /sys/class/gpio/unexport
--- JetPack-6/test_key_b_agx_orin.sh
+++ JetPack-6/test_key_b_agx_orin.sh
@@ -7,33 +7,40 @@ fi
 #gpiochip0 - tegra234-gpio
 #gpiochip1 - tegra234-gpio-aon
 
-gpioset --mode=signal `gpiofind "PX.06"`=0 &
+# rev-1.2 or newer
+gpioset --mode=signal `gpiofind "PY.04"`=0 &
+# rev-1.1 or older
+#gpioset --mode=signal `gpiofind "PX.06"`=0 &
 PID_M2B_USB_SELECT=$!
+
 #gpioset --mode=signal `gpiofind "PP.06"`=0 &
 #PID_M2B_RESET=$!
 gpioset --mode=signal `gpiofind "PA.01"`=1 &
-PID_M2B_PWR_OFF=$!
+PID_M2B_PWR_ON=$!
 #gpioset --mode=signal `gpiofind "PA.02"`=1 &
 #PID_M2B_FULLCARD_PWRON=$!
 #gpioset --mode=signal `gpiofind "PQ.04"`=1 &
-#PID_M2B_DISABLE1=$!
+#PID_M2B_ENABLE1=$!
 #gpioset --mode=signal `gpiofind "PP.04"`=1 &
-#PID_M2B_DISABLE2=$!
+#PID_M2B_ENABLE2=$!
 
 trap interrupt_func INT
 interrupt_func() {
 	kill $PID_M2B_USB_SELECT
 	sleep 0.1
-	gpioset --mode=signal `gpiofind "PX.06"`=1 &
+	# rev-1.2 or newer
+	gpioset --mode=signal `gpiofind "PY.04"`=1 &
+	# rev-1.1 or older
+	#gpioset --mode=signal `gpiofind "PX.06"`=1 &
 	PID_M2B_USB_SELECT=$!
 	sleep 1
 
 	kill $PID_M2B_USB_SELECT
 	#kill $PID_M2B_RESET
-	kill $PID_M2B_PWR_OFF
+	kill $PID_M2B_PWR_ON
 	#kill $PID_M2B_FULLCARD_PWRON
-	#kill $PID_M2B_DISABLE1
-	#kill $PID_M2B_DISABLE2
+	#kill $PID_M2B_ENABLE1
+	#kill $PID_M2B_ENABLE2
 }
 
 watch -n 0.1 lsusb
